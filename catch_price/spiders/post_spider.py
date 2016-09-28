c3b02e4ee87f2b898fde1e08ba84587c8b9efccb
Yang Cun <yangcun@live.com>
2016-09-28 22:00:12 +0800

    fix a bug: the new post datetime has no date

--- catch_price/spiders/post_spider.py
+++ catch_price/spiders/post_spider.py
@@ -25,27 +25,40 @@ class PostSpider(scrapy.Spider):
             for u in self.start_urls:
                 yield scrapy.Request(u, headers=self.headers, cookies=self.cookies, callback=self.parse, dont_filter=True)
 
+    def str2datetime(self, datetime_str):
+        full_datetime_str = ""
+        if len(datetime_str) == 5: # 13:31
+            full_datetime_str = datetime.now().strftime('%Y-%m-%d ') + datetime_str
+        elif len(datetime_str) == 11: # 09-25 20:28
+            full_datetime_str = datetime.now().strftime('%Y-') + datetime_str
+        return datetime.strptime(full_datetime_str, '%Y-%m-%d %H:%M')
+
+    def datetime2str(self, dt):
+        return dt.strftime('%Y-%m-%d %H:%M')
+
     def parse(self, response):
         if response.url not in self.post_datetime_dic:
-            self.post_datetime_dic[response.url] = datetime.strptime('01-01 01:01', '%m-%d %H:%M')
+            self.post_datetime_dic[response.url] = datetime.now()
 
         datetime_str = response.css('.feed-block-extras').xpath('text()').extract_first().strip()
-        new_datetime = datetime.strptime(datetime_str, '%m-%d %H:%M')
-
+        new_datetime = self.str2datetime(datetime_str)
+        print(new_datetime)
+        print(self.post_datetime_dic[response.url])
+        print("new_datetime: {0}, current_datetime: {1}".format(self.datetime2str(new_datetime), self.datetime2str(self.post_datetime_dic[response.url])))
         if new_datetime > self.post_datetime_dic[response.url]:
             self.post_datetime_dic[response.url] = new_datetime
             logging.warning('{0}: new price!!!!!!!'.format(response.status))
+            print('{0}: new price!!!!!!!'.format(response.status))
             SendEmail().send(self.get_name_by_url(response.url), response.url)
             logging.info('sent email')
+            print('sent email')
         else:
             logging.info('{0}: same...'.format(response.status))
+            print('{0}: same...'.format(response.status))
 
         # Tell the admin I'm fine
         if datetime.now().hour == 6 and datetime.now().minute < 20:
             SendEmail().send("I'm the smzdm spider, and I just want to tell you I'm fine, take care.", '')
-
-        # for u in self.start_urls:
-        #     yield scrapy.Request(u, headers=self.headers, cookies=self.cookies, callback=self.parse, dont_filter=True)
 
     def get_name_by_url(self, url):
         dic = {
